9f99be0b7c943c6d1ccec6e0590b4a05cae8e3b5
Max Epperson <maxepperson@gmail.com>
2025-04-07 22:02:26 -0600

    Add arrays storing string values for gender, pronouns, callings, limb type, locations, and factions to mixed-messages.js

---
+++ mixed-messages.js
@@ -0,0 +1,8 @@
+const genderArr = ["man", "woman", "nonbinary", "agender"];
+const pronounsArr = [["he", "him", "his"], ["she", "her", "hers"], ["they", "them", "theirs"], ["it", "it", "its"]];
+
+const callingArr = ["apostle", "arconaut", "greybeard", "gunslinger", "marauder", "pilgrim", "nomad", "scholar", "tinker", "warden", "water merchant", "watervine farmer", ""];
+const limbTypeArr = ["arm", "claw", "hoof", "wing", "tentacle", "fin", "fungal outcrop"];
+
+const locationArr = ["Asphalt Mines", "Bethesda Susa", "Bey Lah", "Eyn Roj", "Ezra", "Garden of Geth", "Golgotha", "Grit Gate", "Gyl", "Joppa", "Kyakukya", "Omonporch", "Red Rock", "ruins of Joppa", "Rust Wells", "Rusted Archway", "Six Day Stilt", "Stiltgrounds", "the hydropon", "Thin World", "Tomb of the Eaters", "Trembling Dunes", "waterlogged tunnel", "Yd Freehold"];
+const factionArr = ["Children of Mamon", "Consortium of Phyta", "Chavvah", "Cult of the Coiled Lamb", "Daughters of Exile", "dromad merchants", "canniabls", "Barathrumites", "Farmer's Guild", "Fellowship of Wardens", "Glow-Wights", "grazing hedonists", "Gyre wights", "hindren of Bey Lah", "Issachari tribe", "Mechanamists", "Merchants' Guild", "Naphtaali tribe", "pariahs", "Putus Templar", "Seekers of the Sightless Way", "Sultan cult", "water barons"];
