6cce851e4b3cb0a3c2919225b8848fe35413a528
Max Epperson <maxepperson@gmail.com>
2025-04-26 18:54:58 -0600

    Add character object factory with multiple properties that align with arrays and methods to convert string to titlecase, generate title of creature, and generate difficulty of creature

--- mixed-messages.js
+++ mixed-messages.js
@@ -5,8 +5,38 @@ const callingArr = ["apostle", "arconaut", "greybeard", "gunslinger", "marauder"
 const limbTypeArr = ["arm", "claw", "hoof", "wing", "tentacle", "fin", "fungal outcrop"];
 
 const locationArr = ["Asphalt Mines", "Bethesda Susa", "Bey Lah", "Eyn Roj", "Ezra", "Garden of Geth", "Golgotha", "Grit Gate", "Gyl", "Joppa", "Kyakukya", "Omonporch", "Red Rock", "ruins of Joppa", "Rust Wells", "Rusted Archway", "Six Day Stilt", "Stiltgrounds", "the hydropon", "Thin World", "Tomb of the Eaters", "Trembling Dunes", "waterlogged tunnel", "Yd Freehold"];
-const factionArr = ["Children of Mamon", "Consortium of Phyta", "Chavvah", "Cult of the Coiled Lamb", "Daughters of Exile", "dromad merchants", "canniabls", "Barathrumites", "Farmer's Guild", "Fellowship of Wardens", "Glow-Wights", "grazing hedonists", "Gyre wights", "hindren of Bey Lah", "Issachari tribe", "Mechanamists", "Merchants' Guild", "Naphtaali tribe", "pariahs", "Putus Templar", "Seekers of the Sightless Way", "Sultan cult", "water barons"];
+const factionArr = ["Children of Mamon", "Consortium of Phyta", "Chavvah", "Cult of the Coiled Lamb", "Daughters of Exile", "dromad merchants", "cannibals", "Barathrumites", "Farmer's Guild", "Fellowship of Wardens", "Glow-Wights", "grazing hedonists", "Gyre wights", "hindren of Bey Lah", "Issachari tribe", "Mechanamists", "Merchants' Guild", "Naphtaali tribe", "pariahs", "Putus Templar", "Seekers of the Sightless Way", "Sultan cult", "water barons"];
 const affinityArr = ["friendly", "neutral", "hostile"];
 const healthStatusArr = ["Perfect", "Fine", "Injured", "Wounded", "Badly Wounded"];
 const statusEffectsArr = ["asleep", "blind", "cleaved", "confused", "covered in liquid", "dazed", "deep dreaming", "dioriented", "emboldened", "frenzied", "frozen", "gleaming", "greased", "hobbled", "inspired", "lost", "lovesick", "mutating", "overburdened", "paralyzed", "phased", "phosphorescent", "prone", "proselytized", "psionocially cleaved", "shaken", "shamed", "shimmering", "sitting", "sluggish", "sprinting", "stained by liquid", "stuck", "stunned", "stunned by gas", "submerged", "syphoned", "terrified", "wakeful"];
 const difficultyArr = ["Easy", "Average", "Tough", "Very Tough", "Impossible"];
+
+const characterFactory = (gender, pronouns, calling, limbType, location, faction, affinity, healthStatus, statusEffects, difficulty) => {
+    return {
+        gender,
+        pronouns,
+        calling,
+        limbType,
+        location,
+        faction,
+        affinity,
+        healthStatus,
+        statusEffects,
+        difficulty,
+
+        titleCase(str) {
+            return str.toLowerCase().split(' ').map(function (word) {
+                return (word.charAt(0).toUpperCase() + word.slice(1));
+            }).join(' ');
+        },
+
+        generateTitle() {
+            const title = `${this.calling}`;
+        },
+
+        generateDifficulty() {
+            return `${titleCase(this.affinity)}, ${titleCase(difficulty)}`;
+        }
+
+    }
+}
